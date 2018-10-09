a053168851408c1a5f486877b927f9f2337b38a8
Rok Novosel <novosel.rok@gmail.com>
2018-10-09 10:25:08 +0200

    Update gen.py with instructions for executing.

--- nbody/data/gen.py
+++ nbody/data/gen.py
@@ -8,6 +8,7 @@ MAX_VELOCITY = 3E03
 MIN_MASS = 1E19
 MAX_MASS = 1E21
 
+# How to run: python3 gen.py <output filename> <# bodies> -> python3 gen.py out1000.txt 1000
 def main():
     fname = sys.argv[1]
     nbodies = int(sys.argv[2])
